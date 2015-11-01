ef0346405e55707a2ccdebfbd41b7c7aa11fc2d9
Cody Bouscaren <codybousc@gmail.com>
2015-11-01 08:01:15 -0500

    Initial File Structure Set

---
+++ js/StudentsController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+  $scope.students = [
+    {
+      name: "Sam Schmidt",
+      name: "Tbone Williams",
+      name: "Sandy Smith"
+    }
+  ]
+}); 

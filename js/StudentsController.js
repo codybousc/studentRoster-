9581a8266b80cd13ef3e36a508c23c9607c13dde
Cody Bouscaren <codybousc@gmail.com>
2015-11-01 10:20:25 -0500

    Ability to add Student/Edit Student Added

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,9 +1,14 @@
-studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+studentRoster.controller('StudentsController', function StudentsController($scope) {
   $scope.students = [
-    {
-      name: "Sam Schmidt",
-      name: "Tbone Williams",
-      name: "Sandy Smith"
-    }
-  ]
-}); 
+    {name: "Tbone Willis"},
+    {name: "Tbone Walker"}
+  ];
+  $scope.addStudent = function() {
+    $scope.students.push({name: $scope.studentName});
+    $scope.studentName = null; //clears input box
+  };
+  $scope.deleteStudent = function(student) {
+    var index = $scope.students.indexOf(student);
+    $scope.students.splice(index, 1);
+  };
+});
